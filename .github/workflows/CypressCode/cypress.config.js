b22e820057065359eff3223c81340df1fb46479e
Apoorva Joshi <apoorva.joshi@cimet.com.au>
2025-02-17 10:51:25 +0530

    TEST

--- .github/workflows/CypressCode/cypress.config.js
+++ .github/workflows/CypressCode/cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'jghwbq',
+  projectId: '4adpqm',
   e2e: {
+    supportFile: false,
     specPattern: 'cypress/e2e/**/*.spec.js',
     experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
